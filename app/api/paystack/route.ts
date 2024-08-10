ed3cbf9ef16ea6fa2cf0088d70ecc5fc6c853f8f
Philani Ncube <ncbphi001@gmail.com>
2024-08-10 12:04:53 +0200

    chore: Add Paystack webhook route for charge success event

---
+++ app/api/paystack/route.ts
@@ -0,0 +1,79 @@
+import { createClient } from "@/utils/supabase/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+type ChargeWebhook = {
+  event: "charge.success";
+  data: {
+    id: number;
+    domain: string;
+    status: string;
+    reference: string;
+    amount: number;
+    message: string | null;
+    gateway_response: string;
+    paid_at: string;
+    created_at: string;
+    channel: string;
+    currency: string;
+    ip_address: string;
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    metadata: any;
+    log: {
+      time_spent: number;
+      attempts: number;
+      authentication: string;
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      errors: any;
+      success: boolean;
+      mobile: boolean;
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      input: any;
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      channel: any;
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      history: any;
+    };
+
+    }
+    fees: number | null;
+    customer: {
+      id: number;
+      first_name: string;
+      last_name: string;
+      email: string;
+      customer_code: string;
+      phone: string | null;
+      metadata: null;
+      risk_action: string;
+    }
+  }
+
+
+
+export async function POST(req: Request) {
+  const supabase = createClient();
+  const reqHeaders = headers();
+  const { createHmac } = await import("node:crypto");
+
+  const body: ChargeWebhook = await req.json();
+
+  const hash = createHmac("sha512", process.env.PAYSTACK_PRIVATE_SECRECT).update(JSON.stringify(body)).digest("hex");
+
+ const paystackSignature = reqHeaders.get("x-paystack-signature");
+
+  if (hash !== paystackSignature) {
+    return new Response("Invalid signature", { status: 400 });
+  }
+
+  console.log(body);
+
+
+return NextResponse.json({
+  status: "success",
+  message: "Webhook received",
+  data: body,
+})
+
+
+}
--- utils/actions/purchase.ts
+++ utils/actions/purchase.ts
@@ -5,87 +5,130 @@ import type { CartProduct } from "@/stores/cart-store";
 import { redirect } from "next/navigation";
 
 import { z } from "zod";
-
+import { createClient } from "../supabase/server";
+import { getCurrentUser } from "../queries/users";
 
 type InitializeResponse = {
-  status: boolean;
-  message: string;
-  data: {
-    authorization_url: string;
-    access_code: string;
-    reference: string;
-  }
-}
+	status: boolean;
+	message: string;
+	data: {
+		authorization_url: string;
+		access_code: string;
+		reference: string;
+	};
+};
 
-export async function checkoutAction( formData:FormData, order_items:CartProduct[]) {
+export async function checkoutAction(
+	formData: FormData,
+	order_items: CartProduct[],
+) {
+	const user = await getCurrentUser();
 
-  const email = formData.get("email") as string;
-  const phone_number = formData.get("phone_number") as string;
-  const first_name = formData.get("first_name") as string;
-  const last_name = formData.get("last_name") as string;
-  const street_address = formData.get("street_address") as string;
-  const city = formData.get("city") as string;
-  const postal_code = formData.get("postal_code") as string;
-  const total_amount = formData.get("total_amount") as string;
+	if (!user) {
+		throw new Error("User not found");
+	}
 
-  const validatedFields = orderSchema.safeParse({
-    email,
-    phone_number,
-    first_name,
-    last_name,
-    street_address,
-    city,
-    postal_code,
-    total_amount,
-  })
+	const email = formData.get("email") as string;
+	const phone_number = formData.get("phone_number") as string;
+	const first_name = formData.get("first_name") as string;
+	const last_name = formData.get("last_name") as string;
+	const street_address = formData.get("street_address") as string;
+	const city = formData.get("city") as string;
+	const postal_code = formData.get("postal_code") as string;
+	const total_amount = formData.get("total_amount") as string;
 
-  console.log(JSON.stringify(validatedFields, null, 2))
+	const validatedFields = orderSchema.safeParse({
+		email,
+		phone_number,
+		first_name,
+		last_name,
+		street_address,
+		city,
+		postal_code,
+		total_amount,
+	});
 
-  if (!validatedFields.success) {
-    throw new Error("Invalid fields");
-  }
+	console.log(JSON.stringify(validatedFields, null, 2));
 
-  const paystack_url = "https://api.paystack.co/transaction/initialize";
+	if (!validatedFields.success) {
+		throw new Error("Invalid fields");
+	}
 
-  const req = await fetch(paystack_url, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${process.env.PAYSTACK_PRIVATE_SECRECT}`,
+	const paystack_url = "https://api.paystack.co/transaction/initialize";
+
+	const req = await fetch(paystack_url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${process.env.PAYSTACK_PRIVATE_SECRECT}`,
+		},
+		body: JSON.stringify({
+			email: validatedFields.data.email,
+			amount: validatedFields.data.total_amount * 100,
+			channels: ["card", "eft"],
+			currency: "ZAR",
+			callback_url: `${process.env.BASE_URL}/checkout/result/success`,
+			metadata: {
+				cancel_url: `${process.env.BASE_URL}/checkout/result/cancelled`,
+				custom_fields: order_items.map((item) => ({
+					item_name: item.title,
+					item_id: item.id,
+					item_price: item.price,
+					item_quantity: item.quantity,
+					item_image: item.product_images[0].image_url,
+					item_slug: item.slug,
+				})),
 			},
-			body: JSON.stringify({
-				email: validatedFields.data.email,
-				amount: validatedFields.data.total_amount * 100,
-				channels: ["card", "eft"],
-				currency: "ZAR",
-				callback_url: `${process.env.BASE_URL}/checkout/result/success`,
-				metadata: {
-					cancel_url: `${process.env.BASE_URL}/checkout/result/cancelled`,
-					custom_fields: order_items.map((item) => ({
-						item_name: item.title,
-						item_id: item.id,
-						item_price: item.price,
-						item_quantity: item.quantity,
-						item_image: item.product_images[0].image_url,
-						item_slug: item.slug,
-					})),
-				},
-			}),
-		})
+		}),
+	});
 
+	const paystack_response: InitializeResponse = await req.json();
 
-  const data :InitializeResponse = await req.json();
+	console.log(JSON.stringify(paystack_response, null, 2));
 
-  console.log(JSON.stringify(data, null, 2))
+	if (paystack_response.status === false) {
+		throw new Error(paystack_response.message);
+	}
 
-  if(data.status === false) {
-    throw new Error(data.message);
-  }
+	const supabase = createClient();
 
+	// create a shipping address
+	const { data: shipping_address, error: shipping_error } = await supabase
+		.from("shipping_addresses")
+		.insert([
+			{
+				street_address: street_address,
+				city: city,
+				postal_code: postal_code,
+				user_id: user.id,
+			},
+		])
+		.select("*")
+		.single();
 
-  // if the status is true, redirect to the authorization_url
-  redirect(data.data.authorization_url)
+	if (shipping_error) {
+		throw new Error(shipping_error.message);
+	}
 
+	// create an order
+	const { data: order, error: order_error } = await supabase
+		.from("orders")
+		.insert([
+			{
+				email: email,
+				phone_number: phone_number,
+				first_name: first_name,
+				last_name: last_name,
+				total_amount: Number(total_amount),
+				shipping_address_id: shipping_address.id,
+				user_id: user.id,
+				status: "PENDING",
+				transaction_id: paystack_response.data.reference,
+			},
+		]).select("*").single();
 
+    console.log(JSON.stringify(order, null, 2));
 
+	// if the status is true, redirect to the authorization_url
+	redirect(paystack_response.data.authorization_url);
 }
