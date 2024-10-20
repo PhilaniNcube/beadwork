857398a0b7b0aa2891a3157d11f0078cd2f140c0
Philani Ncube <ncbphi001@gmail.com>
2024-10-20 17:49:23 +0200

    refactor: Comment out HomepageAbout component in Home page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,5 @@
 import CategoriesSection from "./_components/home/categories-section";
 import FeaturedProducts from "./_components/home/featured-products";
-import HomepageAbout from "./_components/home/homepage-about";
 import HomePageCallToAction from "./_components/home/homepage-call-to-action";
 import HomepageHero from "./_components/home/homepage-hero";
 
@@ -9,7 +8,6 @@ export default async function Home() {
   return (
     <div className="">
      <HomepageHero />
-     {/* <HomepageAbout /> */}
      <FeaturedProducts />
      <CategoriesSection />
      <HomePageCallToAction />
