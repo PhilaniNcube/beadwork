11719aafc0df858d1a8f1edabcb3b53e8c969133
Philani Ncube <ncbphi001@gmail.com>
2024-11-11 19:37:01 +0200

    Enhance product component: Add structured data for SEO using JSON-LD

--- app/(public)/products/[slug]/_components/product.tsx
+++ app/(public)/products/[slug]/_components/product.tsx
@@ -10,10 +10,33 @@ const Product = async ({ slug }:{slug:string}) => {
          <div className="container py-8">Could not retrieve product data</div>
        );
      }
-
      const sizes = await getProductSizes(product.id);
 
+     const jsonLd = {
+        "@context": "https://schema.org/",
+        "@type": "Product",
+        name: product.title,
+        image: product.product_images[0],
+        description: product.description,
+        sku: product.id,
+        offers: {
+          "@type": "Offer",
+          price: product.price,
+          priceCurrency: "ZAR",
+          availability: "https://schema.org/InStock",
+          url: `https://www.glambeads.co.za/products/${slug}`,
+        },
+     }
 
-  return <ProductDetails product={product} sizes={sizes} />;
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <ProductDetails product={product} sizes={sizes} />
+    </>
+  );
 };
 export default Product;
